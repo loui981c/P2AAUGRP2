8c32aa2f417ca9384a715527544fcd36c06236bd
BenjaminVilladsen <benmicvil@gmail.com>
2022-03-29 12:35:05 +0200

    altered button

--- public/scripts/homepage.js
+++ public/scripts/homepage.js
@@ -2,7 +2,7 @@ document.getElementById("create-username").style.display = "none";
 
 function show(x) {
     if (x === 1) {
-        document.getElementById("create-username").style.display = "block";
+        document.getElementById("create-username").style.display = "inline-block";
     }
 } 
 
