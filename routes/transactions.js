7d17946ac80e0cb07f465be626f64756efe1cb00
BenjaminVilladsen <benmicvil@gmail.com>
2022-03-30 15:28:14 +0200

    transaction prettier now

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,10 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-const funMoney = [{main: "FunMoney", sub: "Beer", price: 1000, date: 1}, {main: "FunMoney", sub: "Pizza", price: 69, date: 4}, {main: "FunMoney", sub: "200 shots", price: 420, date: 6}]
-const rent = [{main: "Rent", sub: "Beer", price: 1000, date: 2}, {main: "Rent", sub: "Pizza", price: 69, date: 3}, {main: "Rent", sub: "200 shots", price: 420, date: 5}]
 
-let allTransactions = [{main: "FunMoney", sub: "Beer", price: 1000, date: 3}, {main: "FunMoney", sub: "Pizza", price: 69, date: 4}, {main: "FunMoney", sub: "200 shots", price: 420, date: 6}, {main: "Rent", sub: "Beer", price: 1000, date: 2}, {main: "Rent", sub: "Pizza", price: 69, date: 3}, {main: "Rent", sub: "200 shots", price: 420, date: 5}]
+let allTransactions = [{main: "FunMoney", sub: "Beer", price: 1000, date: 3}, {main: "FunMoney", sub: "Pizza", price: 69, date: 4}, {main: "FunMoney", sub: "200 shots", price: 420, date: 6}, {main: "Rent", sub: "Beer", price: 1000, date: 2}, {main: "Rent", sub: "Pizza", price: 69, date: 1}, {main: "Rent", sub: "200 shots", price: 420, date: 5}]
 
 
 
@@ -21,6 +19,8 @@ router.get('/', function(req, res, next) {
       categories.push(allTransactions[i].main)
     }
   }
+
+  allTransactions.sort((a, b) => a.date - b.date)
   
   res.render("transactions", {categories: categories, allTransactions: allTransactions});
 });
