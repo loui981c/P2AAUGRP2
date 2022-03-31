756a1bc66a63f607f30c3fd5eb2d3e1dd4e9af86
BenjaminVilladsen <benmicvil@gmail.com>
2022-03-31 12:54:05 +0200

    transactions
    You can add transactions, but not read yet tho

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,5 +1,7 @@
 var express = require('express');
+const transactionSchema = require('../schemas/transactionSchema');
 var router = express.Router();
+const Transaction = require("../schemas/transactionSchema")
 
 /* used in transaction.ejs */
 let allTransactions = [{main: "FunMoney", sub: "Beer", price: 1000, date: 3}, {main: "FunMoney", sub: "Pizza", price: 69, date: 4}, {main: "FunMoney", sub: "200 shots", price: 420, date: 6}, {main: "Rent", sub: "Beer", price: 1000, date: 2}, {main: "Rent", sub: "Pizza", price: 69, date: 1}, {main: "Rent", sub: "200 shots", price: 420, date: 5}]
@@ -39,4 +41,16 @@ router.get("/add", (req,res)=>{
   }
 
   res.render("transactions_add", {categories: categories});
+
+})
+
+router.post("/add", (req,res)=>{
+  let transaction = new Transaction(req.body)
+  transaction.save().then(item => {
+    console.log("saved to database")
+  }).catch((err)=>{
+    res.status(400).send("something went wrong when saving to database")
+  })
+  console.log(transaction);
+  res.redirect("/transactions")
 })
--- schemas/transactionSchema.js
+++ schemas/transactionSchema.js
@@ -1,8 +1,8 @@
 const { default: mongoose } = require("mongoose");
 
 var transactionSchema = new mongoose.Schema({
-    main: String,
-    sub: String,
+    mainCategory: String,
+    subCategory: String,
     price: Number,
     date: {type: Date, default: Date.now()}
    });
