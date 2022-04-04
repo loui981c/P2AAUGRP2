ea1df8ec790dc0b3f6fb2cb4c6f66e009f30e401
BenjaminVilladsen <benmicvil@gmail.com>
2022-04-04 20:32:01 +0200

    played around with budget
    The budget is now created from transactions. We need to make it possible to add a category and a respective expected value

--- routes/budget.js
+++ routes/budget.js
@@ -2,6 +2,7 @@ var express = require('express');
 const { all } = require('./transactions');
 var router = express.Router();
 const budgetSchema = require("../schemas/budgetSchema");
+const Transaction = require("../schemas/transactionSchema") 
 
 /* hardcoded categories */
 let allCategories = [{category: 'Food', expected: 3000, spent: 1200, remaining: 0},
@@ -17,7 +18,50 @@ console.log(allCategories[1].category.toLowerCase())
 
 /* GET budget page. */
 router.get('/', function(req, res, next) {
-    
+
+
+     //fetch transactions from database
+  Transaction.find((err, trans)=>{
+    if(!err)
+    {
+        //array of all the categories. This will be used for calculating budget data
+        //we have some prefixed categories
+        let tempCategories = ["Rent", "Savings", "Food", "Income", "Subs", "Fun", "Misc."]
+
+       //if any other category is found, add it to the temp array
+        for(let i = 0; i<trans.length; i++)
+        {
+           if(!tempCategories.includes(trans[i].mainCategory))
+          {
+            tempCategories.push(trans[i].mainCategory)
+          }
+        }
+        console.log(tempCategories)
+
+        //this is where the data found in the for loops below will be stored
+        let budgetDataStuff = [];
+        
+        //find the amount spent on each category by iterating through each trans and summing up all .price values
+        let catExpected = 5000;
+        for (cat of tempCategories){
+            console.log(cat)
+            let totalCatSpendage = 0;
+            for (t of trans)
+            {
+                if (t.mainCategory == cat)
+                {
+
+                    totalCatSpendage += t.price;
+                }
+                //now the total price of the cat is pushed into the budgetDataStuff array along with the category name, a dummy expected value
+            }
+            
+            let catRemaining = catExpected - totalCatSpendage;
+            budgetDataStuff.push({category: cat, expected: catExpected, spent: totalCatSpendage, remaining: catRemaining})
+            catExpected += 200;
+        }
+        console.log(budgetDataStuff);
+    /*
     // get categories from budget
     let categories = [];
     for (let i = 0; i < allCategories.length; i++) {
@@ -53,9 +97,15 @@ router.get('/', function(req, res, next) {
         if (a.category > b.category) { return 1; }
         else { return 0 ;}
     });
+    */
 
-    res.render("budget", {categories: categories, allCategories: allCategories, 
-        totalRemaining: remainingTotal, totalSpent: spent});
+    // res.render("budget", {categories: categories, allCategories: allCategories, 
+    //     totalRemaining: remainingTotal, totalSpent: spent});
+    res.render("budgetVeeTwo", {budgetDataStuff: budgetDataStuff});
+//end of transaction.find
+    }
+})
+//
 });
 
 router.get('/add', function(req, res, next) {
