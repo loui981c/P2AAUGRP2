7846b24c1a69b8cc0eafbccfa8a4cbb4c4f4bac2
loui981c <foldoysteffenslouise@gmail.com>
2022-04-06 18:44:56 +0200

    fixes controllers in transaction

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -3,7 +3,8 @@ let Budget = require('../schemas/budgetSchema');
 let async = require('async');
 
 exports.budgetOverview = function(req, res, next) {
-
+    
+    // fetching data from database parallel
     async.parallel({
         transactions: function(callback) {
             Transaction.find(callback);
@@ -14,11 +15,14 @@ exports.budgetOverview = function(req, res, next) {
     }, function(err, results) {
             if (err) { return next(err); }
             
+            // sets the results from the callback functions
             let trans = results.transactions;
             let budget = results.budget;
             
+            // array to save modified data in
             let dataStuff = [];
     
+            // calculates the spent and remaining of each category and pushes it into dataStuff
             for (let i = 0; i < budget.length; i++) {
                 let spendage = 0;
                 for (t of trans) {
@@ -28,17 +32,26 @@ exports.budgetOverview = function(req, res, next) {
                 }
                 let remaining = budget[i].expected - spendage;
                 dataStuff.push({category: budget[i].category, expected: budget[i].expected, spent: spendage, remaining: remaining});
-                console.log(dataStuff);
-                console.log(i);
             }
+
+            // sorts the categories alphabetically 
+            dataStuff.sort((a, b) => {
+                if (a.category < b.category) {return -1; }
+                if (a.category > b.category) {return 1; }
+                return 0;
+            });
+
+            // render with the data from dataStuff
             res.render("budget", { dataStuff: dataStuff });
     });
 };
 
 exports.AddBudget = function(req, res) {
+    // defining a new schema and boolean
     let newBudget = new Budget(req.body);
     let alreadyExists = false;
 
+    // checks if a category name is already in data 
     Budget.find((err, budget) => {
         if (!err) {
             for (let i = 0; i < budget.length; i++){
@@ -47,6 +60,8 @@ exports.AddBudget = function(req, res) {
                 }
             }
         }
+
+        // not saving the data from user if a category is already existing 
         if (!alreadyExists) {
             newBudget.save().then(item => {
                 console.log("saved to database: " + newBudget);
@@ -56,7 +71,7 @@ exports.AddBudget = function(req, res) {
         } else {
             console.log("This category already exists.");
         }
-    
+        
         res.redirect('/budget');
     });
 };
---
+++ controllers/transactionController.js
@@ -0,0 +1,100 @@
+let Transaction = require('../schemas/transactionSchema');
+let Budget = require('../schemas/budgetSchema');
+let async = require('async');
+
+// maybe the let categoriesFromBudget = []; should be global?
+
+exports.transactionOverview = function (req, res, next) {
+
+    async.parallel({
+        transactions: function (callback) {
+            Transaction.find(callback);
+        },
+        budget: function (callback) {
+            Budget.find(callback);
+        },
+    }, function (err, results) {
+        if (err) { return next(err); }
+
+        // getting the data from async function
+        let trans = results.transactions;
+        let budget = results.budget;
+
+        // gets the categories from budget
+        let categoriesFromBudget = [];
+        for (let i = 0; i < budget.length; i++) {
+            categoriesFromBudget.push(budget[i].category)
+        }
+
+        //get first and last day of the current month
+        today = new Date();
+        //first day
+        todayMonth = today.getMonth() + 1;
+        todayYear = today.getFullYear();
+        firstDay = "01";
+
+        if (todayMonth < 10) {
+            todayMonth = "0" + todayMonth;
+        }
+        firstOutputDate = todayYear + "-" + todayMonth + "-" + firstDay;
+
+        //last Day
+        lastDay = new Date(todayYear, todayMonth, 0);
+        lastDay = lastDay.getDate();
+
+        lastOutPutDate = todayYear + "-" + todayMonth + "-" + lastDay;
+
+        transactionsWithCorrectDates = [];
+        for (t of trans) {
+            //convert format of transaction date to one that matches output dates
+            tempDate = t.date
+            tDay = tempDate.getDate();
+            tMonth = tempDate.getMonth() + 1;
+            tYear = tempDate.getFullYear();
+
+            if (tDay < 10) {
+                tDay = "0" + tDay
+            }
+            if (tMonth < 10) {
+                tMonth = "0" + tMonth
+            }
+            correctDate = tYear + "-" + tMonth + "-" + tDay;;
+
+            //add t to transactionsWithCorrectDates if it matches the first or last date of the current month
+            if (correctDate.replaceAll("-", "") >= firstOutputDate.replaceAll("-", "") && correctDate.replaceAll("-", "") <= lastOutPutDate.replaceAll("-", "")) {
+                transactionsWithCorrectDates.push(t);
+
+            }
+        }
+        console.log(transactionsWithCorrectDates);
+
+        // sorts the dates
+        transactionsWithCorrectDates.sort((a, b) =>  b.date - a.date)
+
+        res.render("transactions", {firstDay: firstOutputDate, lastDay: lastOutPutDate, categories: categoriesFromBudget, allTransactions: transactionsWithCorrectDates, currentCategory: "AllCategories"});
+    });
+};
+
+
+exports.addTransactions = function (req, res, next) {
+    async.parallel({
+        budget: function (callback) {
+            Budget.find(callback);
+        },
+    }, function (err, results) {
+        if (err) { return next(err); }
+
+        let budget = results.budget;
+
+        // gets the categories from budget
+        let categoriesFromBudget = [];
+        for (let i = 0; i < budget.length; i++) {
+            categoriesFromBudget.push(budget[i].category)
+        }
+
+        // sorts the array alphabetically
+        categoriesFromBudget.sort()
+
+        res.render("transactions_add", { categories: categoriesFromBudget });
+    });
+};
