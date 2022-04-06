526a4167d89d6a5b8bc025d22226bb30f57f7c5f
Bence <benceszabo657@gmail.com>
2022-04-06 15:01:14 +0200

    colour input at budget

--- schemas/budgetSchema.js
+++ schemas/budgetSchema.js
@@ -3,7 +3,7 @@ const { default: mongoose } = require("mongoose");
 let budgetSchema = new mongoose.Schema ({
     category: String, // dependency on transactions category?
     expected: Number,
-    color: String,
+    colour: String,
 });
 
 module.exports = mongoose.model('Budgets', budgetSchema);
